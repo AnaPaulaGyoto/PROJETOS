2f060c6e5a0e58a1eb033e46c2a38fbe54b192ad
AnaPaulaGyo <anapv.diniz@gmail.com>
2025-11-07 22:03:11 -0300

    atualização

--- TESTE EFECT PARALLAX/app.js
+++ TESTE EFECT PARALLAX/app.js
@@ -10,5 +10,6 @@ const observer= new IntersectionObserver((entries) =>{
     }
   });
 })
-const hiddenElements = document.querySelectorAll('.hidden' )  ;
+const hiddenElements = document.querySelectorAll('.hidden')  ;
 hiddenElements.forEach((el) => observer.observe(el));
+
